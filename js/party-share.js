5bcfce41682cb008dbbf5b009a86d66f979cf3bc
shaolinjr <arthurclp@icloud.com>
2016-05-29 00:00:15 -0300

    Cards refeitos e eventos futuros refeitos

--- js/party-share.js
+++ js/party-share.js
@@ -100,15 +100,4 @@ $('.navbar-collapse ul li a').click(function() {
   }
 });
 
-    // Accordion future events
-
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].onclick = function(){
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
 
